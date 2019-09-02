3d663efdad853035e88820d81b854b2f5da9c24e
JohnieXu <281910378@qq.com>
2019-09-02 16:37:17 +0800

    feat: 添加vue-typescript&&添加wangeditor编辑器

---
+++ vue-typescript/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <editor v-model="editorContent"></editor>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Editor from '@/components/Editor';
+
+@Component({
+  components: {
+    HelloWorld,
+    Editor,
+  },
+  data() {
+    return {
+      editorContent: '',
+    };
+  },
+})
+export default class Home extends Vue {}
+</script>
