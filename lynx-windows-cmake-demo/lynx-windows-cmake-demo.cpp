075e0abf017ac4c7c8bb527ebe0b0ecfc7d33506
JohnieXu <281910378@qq.com>
2026-07-04 21:08:16 +0800

    feat: Add lynx-windows-cmake-demo project

---
+++ lynx-windows-cmake-demo/lynx-windows-cmake-demo.cpp
@@ -0,0 +1,55 @@
+﻿// lynx-windows-cmake-demo.cpp : Minimal Win32 GUI entry point for the application.
+
+#include "lynx-windows-cmake-demo.h"
+#include <windows.h>
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
+{
+	LPCTSTR CLASS_NAME = "LynxDemoWindowClass";
+	LPCTSTR WINDOW_TITLE = "Lynx Windows CMake Demo";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	if (!RegisterClass(&wc))
+		return 0;
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		WINDOW_TITLE,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, hInstance, NULL);
+
+	if (!hwnd)
+		return 0;
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
